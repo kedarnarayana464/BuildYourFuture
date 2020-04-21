e2e89ab59e2a26203331a097fb3b99942c2cccc0
kedarnarayana464 <kedarnarayana@outlook.com>
2020-04-21 13:31:34 +0530

    Build Your Future Project Start

---
+++ buildUrFuture/src/main/java/com/buildUrFuture/BuildUrFutureApplication.java
@@ -0,0 +1,13 @@
+package com.buildUrFuture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildUrFutureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildUrFutureApplication.class, args);
+	}
+
+}
---
+++ buildUrFuture/src/test/java/com/buildUrFuture/BuildUrFutureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildUrFuture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildUrFutureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
